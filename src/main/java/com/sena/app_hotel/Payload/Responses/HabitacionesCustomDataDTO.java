9de774d5669f99f3e5f27fc85bf3efebc4fe06ce
angieap <aandreaqquina2312@gmail.com>
2025-05-29 21:29:48 -0500

    Refactor HabitacionesCustomDataDTO to improve code structure and maintainability

--- src/main/java/com/sena/app_hotel/Payload/Responses/HabitacionesCustomDataDTO.java
+++ src/main/java/com/sena/app_hotel/Payload/Responses/HabitacionesCustomDataDTO.java
@@ -1,4 +1,22 @@
 package com.sena.app_hotel.Payload.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class HabitacionesCustomDataDTO {
+
+    private Object idHabitacion;
+    private Object nombreHabitacion;
+    private Object tipoHabitacion;
+    private Object estadoHabitacion;
+    private Object precioNoche;
+    private Object imagenUrl;
+    private Object cantidadDisponible;
+
 }
