04602f1254f104c9eef4d8a85fc11bde927d64c7
angieap <aandreaqquina2312@gmail.com>
2025-05-29 21:29:57 -0500

    Refactor ReservasCustomDataDTO to enhance code clarity and maintainability

--- src/main/java/com/sena/app_hotel/Payload/Responses/ReservasCustomDataDTO.java
+++ src/main/java/com/sena/app_hotel/Payload/Responses/ReservasCustomDataDTO.java
@@ -1,4 +1,23 @@
 package com.sena.app_hotel.Payload.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ReservasCustomDataDTO {
+
+    private Object idReserva;
+    private Object idHabitacion;
+    private Object tipoHabitacion;
+    private Object cliente;
+    private Object fechaEntrada;
+    private Object fechaSalida;
+    private Object numeroHuespedes;
+    private Object estadoReserva;
+    private Object fechaReserva;
 }
