35f283645de83c2913edc1a0cea261f4615f7c95
angieap <aandreaqquina2312@gmail.com>
2025-05-29 21:28:43 -0500

    Refactor HabitacionesController to include necessary imports and maintain code structure

--- src/main/java/com/sena/app_hotel/Controller/HabitacionesController.java
+++ src/main/java/com/sena/app_hotel/Controller/HabitacionesController.java
@@ -1,4 +1,27 @@
 package com.sena.app_hotel.Controller;
 
+import com.sena.app_hotel.Exeptions.ObjectResponse;
+import com.sena.app_hotel.Interfaces.IHabitacionesService;
+import com.sena.app_hotel.Interfaces.IReservasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/habitaciones")
+@CrossOrigin("*")
 public class HabitacionesController {
+
+    @Autowired
+    private IHabitacionesService iHabitacionesService;
+
+    // Endpoint para obtener todas las habitaciones
+    @GetMapping("get-all-rooms")
+    public ResponseEntity<ObjectResponse> obtenerTodasLasHabitaciones() {
+        return iHabitacionesService.obtenerTodasLasHabitaciones();
+    }
+
 }
