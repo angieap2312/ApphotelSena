02fed5b62722eb94ea862026a4b0834fb553a862
angieap <aandreaqquina2312@gmail.com>
2025-05-29 21:28:47 -0500

    Add custom query method to HabitacionesRepository for retrieving habitaciones data

--- src/main/java/com/sena/app_hotel/DAO/Repositories/HabitacionesRepository.java
+++ src/main/java/com/sena/app_hotel/DAO/Repositories/HabitacionesRepository.java
@@ -1,8 +1,24 @@
 package com.sena.app_hotel.DAO.Repositories;
 import com.sena.app_hotel.DAO.Entities.HabitacionesEntity;
+import com.sena.app_hotel.Payload.Responses.HabitacionesCustomDataDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface HabitacionesRepository extends JpaRepository <HabitacionesEntity, Long> {
+
+    @Query(value = """
+            SELECT
+              id_habitacion,
+              Nombre_habitacion,
+              tipo_habitacion,
+              estado_habitacion,
+              precio_noche,
+              imagen_url,
+              cantidad_disponible
+            FROM habitaciones
+            """, nativeQuery = true)
+    List<HabitacionesCustomDataDTO> findAllHabitacionesCustomData();
 }
