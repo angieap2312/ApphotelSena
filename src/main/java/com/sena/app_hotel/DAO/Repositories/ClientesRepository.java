78f6c02ee593409a7e871194383224eb8a2f87b9
angieap <aandreaqquina2312@gmail.com>
2025-05-30 22:24:04 -0500

    Refactor ClientesRepository to remove @Repository annotation and adjust import statements for improved clarity

--- src/main/java/com/sena/app_hotel/DAO/Repositories/ClientesRepository.java
+++ src/main/java/com/sena/app_hotel/DAO/Repositories/ClientesRepository.java
@@ -1,8 +1,20 @@
 package com.sena.app_hotel.DAO.Repositories;
-import com.sena.app_hotel.DAO.Entities.ClientesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ClientesRepository extends JpaRepository <ClientesEntity, Long> {
+import com.sena.app_hotel.DAO.Entities.ClientesEntity;
+import com.sena.app_hotel.Payload.Responses.ClientesCustomDataDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ClientesRepository extends JpaRepository<ClientesEntity, Long> {
+
+    @Query(value = """
+            SELECT
+                id_cliente as id,
+                CONCAT(CONCAT(id_cliente, ' - '), nombre, apellido) AS label
+            FROM
+                clientes
+            """, nativeQuery = true)
+    List<ClientesCustomDataDTO> getCustomDataClientes();
 }
