489a05ebdbdd1dd1f6a8fca2a1e598505681414d
angieap <aandreaqquina2312@gmail.com>
2025-05-30 22:23:49 -0500

    Implement ClientesController with endpoint to retrieve all clients

--- src/main/java/com/sena/app_hotel/Controller/ClientesController.java
+++ src/main/java/com/sena/app_hotel/Controller/ClientesController.java
@@ -1,4 +1,25 @@
 package com.sena.app_hotel.Controller;
 
+import com.sena.app_hotel.Exeptions.ObjectResponse;
+import com.sena.app_hotel.Interfaces.IClientesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/clientes")
+@CrossOrigin("*")
+@RequiredArgsConstructor
 public class ClientesController {
+
+    private final IClientesService iClientesService;
+
+    // Endpoint para obtener todas los clientes
+    @GetMapping("get-all-clients")
+    public ResponseEntity<ObjectResponse> obtenerTodosLosClientes() {
+        return iClientesService.getCustomClientes();
+    }
 }
