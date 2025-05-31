b0dbac3b3ffdc68ef7cf3679bb9efeea64b21fd3
angieap <aandreaqquina2312@gmail.com>
2025-05-30 22:24:13 -0500

    Implement ClientesService class with custom client retrieval method

--- src/main/java/com/sena/app_hotel/Services/ClientesService.java
+++ src/main/java/com/sena/app_hotel/Services/ClientesService.java
@@ -1,4 +1,24 @@
 package com.sena.app_hotel.Services;
 
-public class ClientesService {
+import com.sena.app_hotel.DAO.Repositories.ClientesRepository;
+import com.sena.app_hotel.Exeptions.ObjectResponse;
+import com.sena.app_hotel.Interfaces.IClientesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClientesService implements IClientesService {
+
+    private final ClientesRepository clientesRepository;
+
+    @Override
+    public ResponseEntity<ObjectResponse> getCustomClientes() {
+        try {
+            return ResponseEntity.ok().body(new ObjectResponse(0,"El proceso ha terminado con éxito", clientesRepository.getCustomDataClientes()));
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().body(new ObjectResponse(-1,"Ocurrió un error al realizar el proceso " + e.getMessage()));
+        }
+    }
 }
